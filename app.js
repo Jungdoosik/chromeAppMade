16cd2004acfba087b20d45f07aff1f18821df2a0
Doosik Jung <entlrsla@gmail.com>
2022-02-25 16:42:31 +0900

    세번째 커밋

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const amIFat = null;
-
-let something;
-
-console.log(amIFat);sad
+const player = {
+    name : "ddu",
+    points : 10,
+    fat : true, 
+};
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+console.log(player.points);
+console.log(player.fat);
+player.fat = false;
+console.log(player);
+player.lastName = "potato";
+console.log(player);
