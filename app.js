c355706f0e23d588b3a6301afa8ed59db0819761
Doosik Jung <entlrsla@gmail.com>
2022-02-26 13:05:01 +0900

    eventListener

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const player = {
-    name : "ddu",
-    points : 10,
-    fat : true, 
-};
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-console.log(player.points);
-console.log(player.fat);
-player.fat = false;
-console.log(player);
-player.lastName = "potato";
-console.log(player);
+const title = document.querySelector("div.hello h1");
+
+function handleTitleClick(){
+    title.innerHTML = "<h6>Wow Change!!</h6>";
+}
+
+function handMouseEnter(){
+    title.style.color = "blue";
+}
+
+function handMouseLeave(){
+    title.style.color = "red";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handMouseEnter);
+title.addEventListener("mouseleave", handMouseLeave);
