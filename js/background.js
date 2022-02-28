c58555456a3f926926a7bf5c23ee536a6a4e27f6
Doosik Jung <entlrsla@gmail.com>
2022-02-28 19:10:15 +0900

    javascript로 배경추가

---
+++ js/background.js
@@ -0,0 +1,12 @@
+const images = ["배경1.jpg", "배경2.jpg", "배경3.jpg", "배경4.jpg", "배경5.jpg",
+"배경6.jpg", "배경7.jpg", "배경8.jpg", "배경9.jpg", "배경10.jpg"];
+
+const chooseImg = images[Math.floor(Math.random() * images.length)];
+
+const bgImg = document.createElement("img");
+
+bgImg.src = `img/${chooseImg}`;
+
+console.log(bgImg);
+
+document.body.appendChild(bgImg);
--- js/clock.js
+++ js/clock.js
@@ -9,13 +9,16 @@ function getClock(){
 }
 
 getClock();
+//페이지를 시작했을때 setInterval로 반복동작은 줬지만 시작(새로고침,새창이열림)
+//했을때 바로 시간이 나오지 않기 때문에 getClock()을 한번 작동한뒤 반복동작시작
 setInterval(getClock, 1000);
 //setTimeout(getClock, 1000);
 
-//setInterval === 밀리세컨드를 지정해주면 그 시간후에 반복동작
+//setInterval === 밀리세컨드를 지정해주면 그 시간후에 반복동작(실시간)
 //setTimeout === 밀리세컨드를 지정해주면 그 시간후에 동작 (끝)(1회성)
 
-//padStart 
+//padStart (원하는 만큼의 문자의 길이가아니라면 앞쪽에 지정한 문자를 채워넣음)
+//padStart(2,"0") - 2는 원하는 문자길이, "0"은 문자길이가 2가되지않음 0을 넣어
 //getSeconds 를 사용하게 되면 1초일 때 01 아니고 1 만 나옴
 /*
 1
@@ -39,7 +42,6 @@ setInterval(getClock, 1000);
 08
 09
 10 이런식으로 나오게 하기위해 padStart를 사용하는데 이것은 String을 인지하고
-변환하기에 getHours, getMinutes, getSeconds는 number이기때문에 형변환후 
+변환하기에 getHours, getMinutes, getSeconds는 number이기때문에 형변환 후 
 사용한것임
-
 */ 
